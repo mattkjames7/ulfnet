8d24600a7ebeb01d491051ec7d3eeadf01082266
Paolo <s1137563@sms.ed.ac.uk>
2019-06-19 13:35:17 +0100

    Old Network - 152 days - IoU score - 10 epochs- plotting overlay - changed data augm

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import unet_model
-from data_helper import dataGenerator,load_data_Kfold, get_items, test_file_reader, saveResult
+from data_helper import dataGenerator,load_data_Kfold, get_items, test_file_reader, saveResult, plot_imgs
 import ipdb
 import numpy as np
+from keras.callbacks import ModelCheckpoint
+
+
 
 BATCH_SIZE = 2
         
-data_gen_args = dict(rotation_range=0.2,
+data_gen_args = dict(rotation_range=15,
                     width_shift_range=0.05,
                     height_shift_range=0.05,
-                    shear_range=0.05,
-                    zoom_range=0.05,
+                    shear_range=50,
+                    zoom_range=0.2,
                     horizontal_flip=True,
-                    fill_mode='nearest')
+                    vertical_flip=True,
+                    fill_mode='constant')
 
 im_path = 'data/membrane/train/image'
 label_path = 'data/membrane/train/label'
@@ -25,6 +29,14 @@ x_train,x_validation,y_train,y_validation = load_data_Kfold(im_path,label_path,k
 
 #Load model
 model = unet_model.unet()
+model_filename = 'segm_model_v0.h5'
+callback_checkpoint = ModelCheckpoint(
+    model_filename, 
+    verbose=1, 
+    monitor='val_loss', 
+    save_best_only=True,
+)
+
 cv_losses=[]
 cv_acc=[]
 #CV and training
@@ -35,7 +47,7 @@ for fold_number in range(k):
     y_valid = get_items(y_validation[fold_number])
     print(f'Training fold {fold_number}')
     generator = dataGenerator(BATCH_SIZE, x_training,y_training,data_gen_args,seed = 1) 
-    model.fit_generator(generator,steps_per_epoch=len(x_training)/BATCH_SIZE,epochs=1,verbose=1,validation_data = (x_valid,y_valid))
+    model.fit_generator(generator,steps_per_epoch=len(x_training)/BATCH_SIZE,epochs=10,verbose=1,validation_data = (x_valid,y_valid), callbacks=[callback_checkpoint])
    # print(model.evaluate(x_valid, y_valid))
     scores = model.evaluate(x_valid, y_valid)
     print(scores)    
@@ -46,6 +58,11 @@ testGen = test_file_reader(im_test)
 
 print("Loss is %.2f%% (+/- %.2f%%)" % (np.mean(cv_losses), np.std(cv_losses)))
 print("Accuracy is %.2f%% (+/- %.2f%%)" % (np.mean(cv_acc), np.std(cv_acc)))
+
+model.load_weights(model_filename)
+y_pred = model.predict(x_valid)
+
+images= plot_imgs(org_imgs=x_valid, mask_imgs=y_valid, pred_imgs=y_pred, nm_img_to_plot=9)
 
 
 results = model.predict_generator(testGen,10,verbose=1)

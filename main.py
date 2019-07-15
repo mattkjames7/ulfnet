1ca01ab63b92527e086ad3e822e1c147f6315ad9
Paolo <s1137563@sms.ed.ac.uk>
2019-07-15 23:12:14 +0100

    Results small scale grid search for batch size and optimizer

--- main.py
+++ main.py
@@ -46,16 +46,28 @@ k = 2
 SEED = 1
 
 #Network hyperparameters
-learning_rate = [0.0001, 0.001, 0.01, 0.1]
-drop_out = np.arange(0.2,0.8,0.1)
-weight_init_mode = ['he_normal','he_uniform','glorot_normal','glorot_uniform','lecun_uniform','normal','uniform']
-batch_size = [2,4,16,32,64]
-optimizer=['SGD', 'Adam']
+learning_rate = [0.0001,0.001]
+drop_out = 0.5
+#weight_init_mode = ['he_normal','he_uniform','glorot_normal','glorot_uniform','lecun_uniform','normal','uniform']
+batch_size = [2,4,16,32]
+optimizer= ['SGD', 'Adam']
+
+
+
+
+#learning_rate = [0.0001, 0.001, 0.01, 0.1]
+#drop_out = np.arange(0.2,0.8,0.1)
+
+
+#NOT USED ANYWAY #weight_init_mode = ['he_normal','he_uniform','glorot_normal','glorot_uniform','lecun_uniform','normal','uniform']
+
+#batch_size = [2,4,16,32,64]
+#optimizer=['SGD', 'Adam']
 
 
 ###CHECK OUTPUT FILE FROM PREVIOUS RUNS TO SEE IF YOU SHOULD CHANGED THESE CALLBACKS
 
-early_stop = EarlyStopping(monitor='val_loss', patience=1, verbose=1, min_delta=0.1)
+early_stop = EarlyStopping(monitor='val_loss', patience=2, verbose=1, min_delta=0.1)
 #reduce_lr = ReduceLROnPlateau(factor=0.3,patience=2, min_lr=0.000001, verbose=1)
 callbacks=[early_stop]
 
@@ -66,9 +78,11 @@ x_train,x_validation,y_train,y_validation = load_data_Kfold(im_path,label_path,k
 
 #Create permutations
 cv_losses = []
-permutations = find_parameters(learning_rate,drop_out, weight_init_mode, batch_size, optimizer)
+permutations = find_parameters(learning_rate,drop_out, batch_size, optimizer)
 
 #Model hyperparameter selection loop
+
+time_before_loop=time.time() 
 for idx_perms in range(len(permutations)):
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
@@ -79,18 +93,18 @@ for idx_perms in range(len(permutations)):
     cv_losses_temp = []
     curr_lr = permutations[idx_perms,0]
     curr_drop_out =  permutations[idx_perms,1]
-    curr_init = permutations[idx_perms,2]
-    curr_batch_size = permutations[idx_perms,3]
+#    curr_init = permutations[idx_perms,2]
+    curr_batch_size = permutations[idx_perms,2]
     curr_batch_size_np=np.array(curr_batch_size, dtype=int)
     #print(type(curr_batch_size))
     #print(type(curr_batch_size_np))
     #print(curr_batch_size_np.shape)
     #print(curr_batch_size)
-    curr_optimizer = permutations[idx_perms,4]
+    curr_optimizer = permutations[idx_perms,3]
     print('\n')
     print('current lr: ' +str(curr_lr))
     print('current dropout: ' +str(curr_drop_out))
-    print('current init: ' +str(curr_init))
+   # print('current init: ' +str(curr_init))
     print('current batch size: ' +str(curr_batch_size_np))
     print('current optimizer: ' +str(curr_optimizer))
     
@@ -100,7 +114,7 @@ for idx_perms in range(len(permutations)):
     #print(f'current batch size: {curr_batch_size}')
     #print(f'current optimizer: {curr_optimizer}')
     #print('\n')
-    model = unet_model.unet(learning_rate=curr_lr.astype(np.float),drop_out=curr_drop_out.astype(np.float),weight_init_mode=curr_init, optimizer = curr_optimizer)    
+    model = unet_model.unet(learning_rate=curr_lr.astype(np.float),drop_out=curr_drop_out.astype(np.float), optimizer = curr_optimizer)    
         
     #Model evaluation loop
     for fold_number in range(k):
@@ -117,7 +131,9 @@ for idx_perms in range(len(permutations)):
         cv_losses_temp.append(scores[0])
     cv_losses.append(np.array(cv_losses_temp).mean())
     tf.reset_default_graph()
-df = pd.DataFrame({'learning rate':permutations[:,0],'drop_out':permutations[:,1],'weight_init':permutations[:,2],'batch_size':permutations[:,3],'optimizer':permutations[:,4], 'val_loss':cv_losses}) 
+#print("--- %s seconds ---" % (time.time() - time_before_loop))
+
+df = pd.DataFrame({'learning rate':permutations[:,0],'drop_out':permutations[:,1],'batch_size':permutations[:,2],'optimizer':permutations[:,3], 'val_loss':cv_losses}) 
  
 df.to_csv(os.path.join("/lustre/home/d167/s1137563/Paolo_repository/unet","output_file.csv"),encoding='utf-8', index=False)
  
@@ -126,6 +142,6 @@ min_loss_conf = df.iloc[df['val_loss'].idxmin()]
 print(min_loss_conf)
 
 
-
+print("--- %s seconds ---" % (time.time() - start_time))
 
 

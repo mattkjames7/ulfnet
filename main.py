19b299a72d8e9175684de188ebb7dc2dcf56ee75
Paolo <s1137563@sms.ed.ac.uk>
2019-07-25 15:36:12 +0100

    Run non exhaustive test with weight init

--- main.py
+++ main.py
@@ -53,11 +53,11 @@ k = 2
 SEED = 1
 
 #Network hyperparameters
-learning_rate = [0.0001,0.001, 0.01]
+learning_rate = 0.0001
 drop_out = 0.5
-#weight_init_mode = ['he_normal','he_uniform','glorot_normal','glorot_uniform','lecun_uniform','normal','uniform']
-batch_size = [2,4,16,32]
-optimizer= ['SGD', 'Adam', 'RMSprop']
+weight_init_mode = ['he_normal','he_uniform','glorot_normal','glorot_uniform','lecun_uniform','normal','uniform']
+batch_size = 2
+optimizer=  'Adam'
 
 
 
@@ -74,7 +74,7 @@ optimizer= ['SGD', 'Adam', 'RMSprop']
 
 ###CHECK OUTPUT FILE FROM PREVIOUS RUNS TO SEE IF YOU SHOULD CHANGED THESE CALLBACKS
 
-early_stop = EarlyStopping(monitor='val_loss', patience=2, verbose=1, min_delta=0.1)
+early_stop = EarlyStopping(monitor='val_loss', patience=2, verbose=1, min_delta=0.01)
 #reduce_lr = ReduceLROnPlateau(factor=0.3,patience=2, min_lr=0.000001, verbose=1)
 callbacks=[early_stop]
 
@@ -85,7 +85,7 @@ x_train,x_validation,y_train,y_validation = load_data_Kfold(im_path,label_path,k
 
 #Create permutations
 cv_losses = []
-permutations = find_parameters(learning_rate,drop_out, batch_size, optimizer)
+permutations = find_parameters(learning_rate,drop_out,weight_init_mode, batch_size, optimizer)
 
 #Model hyperparameter selection loop
 
@@ -100,18 +100,18 @@ for idx_perms in range(len(permutations)):
     cv_losses_temp = []
     curr_lr = permutations[idx_perms,0]
     curr_drop_out =  permutations[idx_perms,1]
-#    curr_init = permutations[idx_perms,2]
-    curr_batch_size = permutations[idx_perms,2]
+    curr_init = permutations[idx_perms,2]
+    curr_batch_size = permutations[idx_perms,3]
     curr_batch_size_np=np.array(curr_batch_size, dtype=int)
     #print(type(curr_batch_size))
     #print(type(curr_batch_size_np))
     #print(curr_batch_size_np.shape)
     #print(curr_batch_size)
-    curr_optimizer = permutations[idx_perms,3]
+    curr_optimizer = permutations[idx_perms,4]
     print('\n')
     print('current lr: ' +str(curr_lr))
     print('current dropout: ' +str(curr_drop_out))
-   # print('current init: ' +str(curr_init))
+    print('current init: ' +str(curr_init))
     print('current batch size: ' +str(curr_batch_size_np))
     print('current optimizer: ' +str(curr_optimizer))
     
@@ -121,7 +121,7 @@ for idx_perms in range(len(permutations)):
     #print(f'current batch size: {curr_batch_size}')
     #print(f'current optimizer: {curr_optimizer}')
     #print('\n')
-   #model = unet_model.unet(learning_rate=curr_lr.astype(np.float),drop_out=curr_drop_out.astype(np.float), optimizer = curr_optimizer)    
+   #model = unet_model.unet(learning_rate=curr_lr.astype(np.float),drop_out=curr_drop_out.astype(np.float), weight_init_mode=curr_init, optimizer = curr_optimizer)    
         
     #Model evaluation loop
     for fold_number in range(k):
@@ -132,9 +132,9 @@ for idx_perms in range(len(permutations)):
         y_valid = get_items(y_validation[fold_number])
         generator = dataGenerator(curr_batch_size_np, x_training,y_training,data_gen_args,seed = SEED)
         value= int(len(x_training)/curr_batch_size_np)
-        model = unet_model.unet(learning_rate=curr_lr.astype(np.float),drop_out=curr_drop_out.astype(np.float), optimizer = curr_optimizer)        
+        model = unet_model.unet(learning_rate=curr_lr.astype(np.float),drop_out=curr_drop_out.astype(np.float),weight_init_mode=curr_init, optimizer = curr_optimizer)        
 #print(value)
-        history = model.fit_generator(generator,steps_per_epoch= value,epochs=10,verbose=1,validation_data = (x_valid,y_valid))#, callbacks=callbacks) #callbacks=callbacks)
+        history = model.fit_generator(generator,steps_per_epoch= value,epochs=10,verbose=1,validation_data = (x_valid,y_valid), callbacks=callbacks) #callbacks=callbacks)
         scores = model.evaluate(x_valid, y_valid)
         cv_losses_temp.append(scores[0])
         K.clear_session()
@@ -153,7 +153,7 @@ for idx_perms in range(len(permutations)):
  #   tf.reset_default_graph()
 #print("--- %s seconds ---" % (time.time() - time_before_loop))
 
-df = pd.DataFrame({'learning rate':permutations[:,0],'drop_out':permutations[:,1],'batch_size':permutations[:,2],'optimizer':permutations[:,3], 'val_loss':cv_losses}) 
+df = pd.DataFrame({'learning rate':permutations[:,0],'drop_out':permutations[:,1], 'weight_init_mode':permutations[:,2],'batch_size':permutations[:,3],'optimizer':permutations[:,4], 'val_loss':cv_losses}) 
  
 df.to_csv(os.path.join("/lustre/home/d167/s1137563/Paolo_repository/unet","output_file.csv"),encoding='utf-8', index=False)
  

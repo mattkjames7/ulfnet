75627c0a1ef794f2f37b0d259dc13ceb16ba0aeb
Paolo <s1137563@sms.ed.ac.uk>
2019-07-28 19:59:24 +0100

    Test with jaccard distance as alternative loss functions

--- unet_model.py
+++ unet_model.py
@@ -22,7 +22,37 @@ def dice_coef(y_true, y_pred, smooth=1.):
     return (2. * intersection + smooth) / (
                 K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
-
+def jaccard_distance(y_true, y_pred, smooth=100):
+    """Jaccard distance for semantic segmentation.
+    Also known as the intersection-over-union loss.
+    This loss is useful when you have unbalanced numbers of pixels within an image
+    because it gives all classes equal weight. However, it is not the defacto
+    standard for image segmentation.
+    For example, assume you are trying to predict if
+    each pixel is cat, dog, or background.
+    You have 80% background pixels, 10% dog, and 10% cat.
+    If the model predicts 100% background
+    should it be be 80% right (as with categorical cross entropy)
+    or 30% (with this loss)?
+    The loss has been modified to have a smooth gradient as it converges on zero.
+    This has been shifted so it converges on 0 and is smoothed to avoid exploding
+    or disappearing gradient.
+    Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
+            = sum(|A*B|)/(sum(|A|)+sum(|B|)-sum(|A*B|))
+    # Arguments
+        y_true: The ground truth tensor.
+        y_pred: The predicted tensor
+        smooth: Smoothing factor. Default is 100.
+    # Returns
+        The Jaccard distance between the two tensors.
+    # References
+        - [What is a good evaluation measure for semantic segmentation?](
+           http://www.bmva.org/bmvc/2013/Papers/paper0032/paper0032.pdf)
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return (1 - jac) * smooth
 
 def unet(pretrained_weights = None,input_size = (256,256,1)):
     inputs = Input(input_size)
@@ -68,7 +98,7 @@ def unet(pretrained_weights = None,input_size = (256,256,1)):
 
 
     model = Model(inputs = inputs, outputs = conv10)
-    model.compile(optimizer = Adam(lr = 1e-4), loss = dice_coef, metrics = ['accuracy', iou])
+    model.compile(optimizer = Adam(lr = 1e-4), loss = jaccard_distance, metrics = ['accuracy', iou])
     
     #model.summary()
 

d282ea6f043a33432b0f0d6c04631b7009c16473
marangiop <marangiopaolo@gmail.com>
2019-11-15 02:42:36 +0100

    Ensuring PEP 8 compliance

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 from keras.preprocessing.image import ImageDataGenerator
-import numpy as np 
+import numpy as np
 import os
 from glob import glob
 import skimage.io as io
@@ -8,27 +8,27 @@ import skimage.transform as trans
 import matplotlib.pyplot as plt
 
 
-def adjustData(img,mask,flag_multi_class,num_class):
+def adjustData(img, mask, flag_multi_class, num_class):
     if(flag_multi_class):
         img = img / 255
-        mask = mask[:,:,:,0] if(len(mask.shape) == 4) else mask[:,:,0]
+        mask = mask[:, :, :, 0] if(len(mask.shape) == 4) else mask[:, :, 0]
         new_mask = np.zeros(mask.shape + (num_class,))
         for i in range(num_class):
-            new_mask[mask == i,i] = 1
-        new_mask = np.reshape(new_mask,(new_mask.shape[0],new_mask.shape[1]*new_mask.shape[2],new_mask.shape[3])) if flag_multi_class else np.reshape(new_mask,(new_mask.shape[0]*new_mask.shape[1],new_mask.shape[2]))
+            new_mask[mask == i, i] = 1
+        new_mask = np.reshape(new_mask, (new_mask.shape[0], new_mask.shape[1]*new_mask.shape[2], new_mask.shape[3])
+                              ) if flag_multi_class else np.reshape(new_mask, (new_mask.shape[0]*new_mask.shape[1], new_mask.shape[2]))
         mask = new_mask
     elif(np.max(img) > 1):
         img = img / 255
-        mask = mask /255
+        mask = mask / 255
         mask[mask > 0.5] = 1
         mask[mask <= 0.5] = 0
-    return (img,mask)
+    return (img, mask)
 
 
-
-def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "grayscale",
-                    mask_color_mode = "grayscale",image_save_prefix  = "image",mask_save_prefix  = "mask",
-                    flag_multi_class = False,num_class = 2,save_to_dir = None,target_size = (256,256),seed = 1):
+def trainGenerator(batch_size, train_path, image_folder, mask_folder, aug_dict, image_color_mode="grayscale",
+                   mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
+                   flag_multi_class=False, num_class=2, save_to_dir=None, target_size=(256, 256), seed=1):
     '''
     can generate image and mask at the same time
     use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
@@ -38,36 +38,36 @@ def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image
     mask_datagen = ImageDataGenerator(**aug_dict)
     image_generator = image_datagen.flow_from_directory(
         train_path,
-        classes = [image_folder],
-        class_mode = None,
-        color_mode = image_color_mode,
-        target_size = target_size,
-        batch_size = batch_size,
-        save_to_dir = save_to_dir,
-        save_prefix  = image_save_prefix,
-        seed = seed)
+        classes=[image_folder],
+        class_mode=None,
+        color_mode=image_color_mode,
+        target_size=target_size,
+        batch_size=batch_size,
+        save_to_dir=save_to_dir,
+        save_prefix=image_save_prefix,
+        seed=seed)
     mask_generator = mask_datagen.flow_from_directory(
         train_path,
-        classes = [mask_folder],
-        class_mode = None,
-        color_mode = mask_color_mode,
-        target_size = target_size,
-        batch_size = batch_size,
-        save_to_dir = save_to_dir,
-        save_prefix  = mask_save_prefix,
-        seed = seed)
+        classes=[mask_folder],
+        class_mode=None,
+        color_mode=mask_color_mode,
+        target_size=target_size,
+        batch_size=batch_size,
+        save_to_dir=save_to_dir,
+        save_prefix=mask_save_prefix,
+        seed=seed)
     train_generator = zip(image_generator, mask_generator)
-    for (img,mask) in train_generator:
-        img,mask = adjustData(img,mask,flag_multi_class,num_class)
-        yield (img,mask)
+    for (img, mask) in train_generator:
+        img, mask = adjustData(img, mask, flag_multi_class, num_class)
+        yield (img, mask)
 
 
 def plot_segm_history(history, things_to_plot=['iou', 'loss']):
-    
-    plt.figure(figsize=(12,6))
+
+    plt.figure(figsize=(12, 6))
     for metric_or_loss in things_to_plot:
         plt.plot(history.history[metric_or_loss], linewidth=3)
-   
+
     plt.suptitle('Plot of Training Loss and IoU', fontsize=20)
     plt.ylabel('Value', fontsize=20)
     plt.xlabel('Number of Epochs', fontsize=20)
@@ -77,33 +77,38 @@ def plot_segm_history(history, things_to_plot=['iou', 'loss']):
     plt.savefig('Plot_training_loss_and_iou.png', format='png', dpi=1000)
 
 
-def testGenerator(test_path,num_image,target_size = (256,256),as_gray = True):
+def testGenerator(test_path, num_image, target_size=(256, 256), as_gray=True):
     for i in range(num_image):
-        img = io.imread(os.path.join(test_path,"%d.jpg"%i),as_gray = as_gray)
+        img = io.imread(os.path.join(test_path, "%d.jpg" % i), as_gray=as_gray)
         #img = img / 255
-        img = trans.resize(img,target_size)
-        img = np.reshape(img,img.shape+(1,))
-        img = np.reshape(img,(1,)+img.shape)
+        img = trans.resize(img, target_size)
+        img = np.reshape(img, img.shape+(1,))
+        img = np.reshape(img, (1,)+img.shape)
         yield img
-     
-def saveResult(save_path,pred_im_array): 
-    #saves images into specified directory
-    for i,item in enumerate(pred_im_array):
-        img = item[:,:,0]
-        io.imsave(os.path.join(save_path,str(i)+"_predict.png"),img)
+
+
+def saveResult(save_path, pred_im_array):
+    # saves images into specified directory
+    for i, item in enumerate(pred_im_array):
+        img = item[:, :, 0]
+        io.imsave(os.path.join(save_path, str(i)+"_predict.png"), img)
+
 
 def threshold_binarize(x, threshold=0.5):
-   
-    y = np.copy(x)    
+
+    y = np.copy(x)
     y[y >= threshold] = 1
     return y
 
-def saveResultThresholded(save_path,pred_im_array,threshold):
-    #saves threshold images into specified directory
+
+def saveResultThresholded(save_path, pred_im_array, threshold):
+    # saves threshold images into specified directory
     results_thresholded = threshold_binarize(pred_im_array, threshold)
-    for i,item in enumerate(results_thresholded):
-        img = item[:,:,0]
-        io.imsave(os.path.join(save_path,str(i)+"_predict_thresholded_" + str(threshold) + ".png"),img)
+    for i, item in enumerate(results_thresholded):
+        img = item[:, :, 0]
+        io.imsave(os.path.join(save_path, str(i) +
+                               "_predict_thresholded_" + str(threshold) + ".png"), img)
+
 
 def reshape_arr(arr):
     if arr.ndim == 3:
@@ -113,6 +118,7 @@ def reshape_arr(arr):
             return arr
         elif arr.shape[3] == 1:
             return arr.reshape(arr.shape[0], arr.shape[1], arr.shape[2])
+
 
 def get_cmap(arr):
     if arr.ndim == 3:
@@ -136,10 +142,10 @@ def mask_to_red(mask):
     Also adds alpha channel to make black background transparent.
     '''
     img_size = mask.shape[0]
-    c1 = mask.reshape(img_size,img_size)
-    c2 = np.zeros((img_size,img_size))
-    c3 = np.zeros((img_size,img_size))
-    c4 = mask.reshape(img_size,img_size)
+    c1 = mask.reshape(img_size, img_size)
+    c2 = np.zeros((img_size, img_size))
+    c3 = np.zeros((img_size, img_size))
+    c4 = mask.reshape(img_size, img_size)
     return np.stack((c1, c2, c3, c4), axis=-1)
 
 
@@ -149,8 +155,8 @@ def mask_to_rgba(mask, color='red'):
     Also adds alpha channel to make black background transparent.
     '''
     img_size = mask.shape[0]
-    zeros = np.zeros((img_size,img_size))
-    ones = mask.reshape(img_size,img_size)
+    zeros = np.zeros((img_size, img_size))
+    ones = mask.reshape(img_size, img_size)
     if color == 'red':
         return np.stack((ones, zeros, zeros, ones), axis=-1)
     elif color == 'green':
@@ -164,26 +170,27 @@ def mask_to_rgba(mask, color='red'):
     elif color == 'cyan':
         return np.stack((zeros, ones, ones, ones), axis=-1)
 
+
 def mask_to_red(mask):
     '''
     Converts binary segmentation mask from white to red color.
     Also adds alpha channel to make black background transparent.
     '''
     img_size = mask.shape[0]
-    c1 = mask.reshape(img_size,img_size)
-    c2 = np.zeros((img_size,img_size))
-    c3 = np.zeros((img_size,img_size))
-    c4 = mask.reshape(img_size,img_size)
+    c1 = mask.reshape(img_size, img_size)
+    c2 = np.zeros((img_size, img_size))
+    c3 = np.zeros((img_size, img_size))
+    c4 = mask.reshape(img_size, img_size)
     return np.stack((c1, c2, c3, c4), axis=-1)
 
 
-def plot_imgs(org_imgs, 
-              mask_imgs, 
-              pred_imgs=None, 
-              nm_img_to_plot=10, 
+def plot_imgs(org_imgs,
+              mask_imgs,
+              pred_imgs=None,
+              nm_img_to_plot=10,
               figsize=4,
               alpha=0.5
-             ):
+              ):
     '''
     Image plotting for semantic segmentation data.
     Last column is always an overlay of ground truth or prediction
@@ -196,36 +203,38 @@ def plot_imgs(org_imgs,
 
     org_imgs = reshape_arr(org_imgs)
     mask_imgs = reshape_arr(mask_imgs)
-    if  not (pred_imgs is None):
+    if not (pred_imgs is None):
         cols = 4
         pred_imgs = reshape_arr(pred_imgs)
     else:
         cols = 3
 
-        
-    fig, axes = plt.subplots(nm_img_to_plot, cols, figsize=(cols*figsize, nm_img_to_plot*figsize))
-    axes[0, 0].set_title("test image", fontsize=15) 
+    fig, axes = plt.subplots(nm_img_to_plot, cols, figsize=(
+        cols*figsize, nm_img_to_plot*figsize))
+    axes[0, 0].set_title("test image", fontsize=15)
     axes[0, 1].set_title("ground truth", fontsize=15)
     if not (pred_imgs is None):
-        axes[0, 2].set_title("prediction thresholded", fontsize=15) 
-        axes[0, 3].set_title("overlay", fontsize=15) 
+        axes[0, 2].set_title("prediction thresholded", fontsize=15)
+        axes[0, 3].set_title("overlay", fontsize=15)
     else:
-        axes[0, 2].set_title("overlay groundtruth prediction", fontsize=15) 
+        axes[0, 2].set_title("overlay groundtruth prediction", fontsize=15)
     for m in range(0, nm_img_to_plot):
         axes[m, 0].imshow(org_imgs[im_id], cmap=get_cmap(org_imgs))
         axes[m, 0].set_axis_off()
         axes[m, 1].imshow(mask_imgs[im_id], cmap=get_cmap(mask_imgs))
-        axes[m, 1].set_axis_off()        
+        axes[m, 1].set_axis_off()
         if not (pred_imgs is None):
             axes[m, 2].imshow(pred_imgs[im_id], cmap=get_cmap(pred_imgs))
             axes[m, 2].set_axis_off()
             axes[m, 3].imshow(pred_imgs[im_id], cmap=get_cmap(pred_imgs))
-            axes[m, 3].imshow(mask_to_red(zero_pad_mask(mask_imgs[im_id], desired_size=org_imgs_size)), cmap=get_cmap(mask_imgs), alpha=alpha)
+            axes[m, 3].imshow(mask_to_red(zero_pad_mask(
+                mask_imgs[im_id], desired_size=org_imgs_size)), cmap=get_cmap(mask_imgs), alpha=alpha)
             axes[m, 3].set_axis_off()
         else:
             axes[m, 2].imshow(org_imgs[im_id], cmap=get_cmap(org_imgs))
-            axes[m, 2].imshow(mask_to_red(zero_pad_mask(mask_imgs[im_id], desired_size=org_imgs_size)), cmap=get_cmap(mask_imgs), alpha=alpha)
+            axes[m, 2].imshow(mask_to_red(zero_pad_mask(
+                mask_imgs[im_id], desired_size=org_imgs_size)), cmap=get_cmap(mask_imgs), alpha=alpha)
             axes[m, 2].set_axis_off()
         im_id += 1
 
-    plt.savefig('predictions_overlay_repeat.png', format='png', dpi=1000)    
+    plt.savefig('predictions_overlay_repeat.png', format='png', dpi=1000)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import unet_model 
+import unet_model
 from data import trainGenerator, testGenerator, saveResult, saveResultThresholded, threshold_binarize, plot_imgs
 import keras
 from keras.callbacks import ModelCheckpoint
@@ -8,30 +8,32 @@ import tensorflow as tf
 
 
 data_gen_args = dict(rotation_range=0.2,
-                        width_shift_range=0.05,
-                        height_shift_range=0.05,
-                        shear_range=0.05,
-                        zoom_range=0.05,
-                        horizontal_flip=True,
-                        fill_mode='nearest')
-                        
-BATCH_SIZE=2
-myGene = trainGenerator(BATCH_SIZE,'/lustre/home/d167/s1137563/Paolo_repository/unet/data/membrane/train','image','label',data_gen_args,save_to_dir = None)
-    
+                     width_shift_range=0.05,
+                     height_shift_range=0.05,
+                     shear_range=0.05,
+                     zoom_range=0.05,
+                     horizontal_flip=True,
+                     fill_mode='nearest')
+
+BATCH_SIZE = 2
+myGene = trainGenerator(BATCH_SIZE, '/lustre/home/d167/s1137563/Paolo_repository/unet/data/membrane/train',
+                        'image', 'label', data_gen_args, save_to_dir=None)
+
 model = unet_model.unet()
-model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-history = model.fit_generator(myGene,steps_per_epoch=178/BATCH_SIZE,epochs=10,callbacks=[model_checkpoint])
+model_checkpoint = ModelCheckpoint(
+    'unet_membrane.hdf5', monitor='loss', verbose=1, save_best_only=True)
+history = model.fit_generator(
+    myGene, steps_per_epoch=178/BATCH_SIZE, epochs=10, callbacks=[model_checkpoint])
 figure = plot_segm_history(history)
 
 im_test = '/lustre/home/d167/s1137563/Paolo_repository/unet/data/membrane/test'
 
-#Since 178 images out of 2313 were used for taining, the test dataset actually only includes 2135 days. For simplicity, predictions were generated for all 2313 days.
+# Since 178 images out of 2313 were used for taining, the test dataset actually only includes 2135 days. For simplicity, predictions were generated for all 2313 days.
 
-NUM_TEST_IMAGES=2313
+NUM_TEST_IMAGES = 2313
 
 testGene = testGenerator(im_test, NUM_TEST_IMAGES)
-results = model.predict_generator(testGene,NUM_TEST_IMAGES,verbose=1)
+results = model.predict_generator(testGene, NUM_TEST_IMAGES, verbose=1)
 
-saveResult(im_test,results)
-saveResultThresholded(im_test,results, threshold=0.5)
-
+saveResult(im_test, results)
+saveResultThresholded(im_test, results, threshold=0.5)
